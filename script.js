07bdae4659426e8627be5d44e673d485ce323fd5
Cmac <mckoyc@gmail.com>
2020-09-21 18:41:36 -0400

    Added if/else statement, created for loop for password

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var alphabetLow = 'abcdefghijklmopqrstuvwxyz';
+var alphabetLow = 'abcdefghijklmnopqrstuvwxyz';
 var alphabetUp = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var Numbers = "0123456789";
-var specChar = [' ','!','"','#','$','%','&','\'','(',')','*','+','\,','<','>','?','@','[',']','\b','^','`','_','{','}','|','~'];
+var specChar = [' ','!','"','#','$','%','&','\'','(',')','*','+','\,','<','>','?','@','[',']','\\','^','`','_','{','}','|','~','.','-','/'];
 var NumChar = Numbers.split("");
 var upperCase = alphabetUp.split('');
 var lowerCase = alphabetLow.split('');
@@ -17,33 +17,24 @@ var passwordText = document.querySelector("#password");
 passwordText.value = password;
 
 }
-
-// Checks that a Number is input
-// function numberChecker(x){
-//  var Checker = lowerCase.includes(x.toLowerCase().charAt(0))
-//  if (Checker === true) {
-//   do {
-//   var Correction = prompt("Please enter a number");
-//   x = Correction
-//   } while (numberChecker(x) === true)
-//  }
    
 
 function generatePassword(){
   var PassLength = prompt("How many characters would you like in your password:");
-  //var AlphaLower = confirm("Would you like to include Lowercase letters?")
-  //var AlphaUpper = confirm("Would you like to include Uppercase letters?");
-  //var Special = confirm("Would you like to include special characters?");
-  //var Numerals = confirm("Would you like to include numerals?")
-  
-  //if (PassLength < 0 ) {
-  //    do {
-  //    var Correction = prompt("Please enter a number");
-  //    x = Correction
-  //    } while (PassLength < 0)
-  //  }
 
-  console.log("Can I push this to GitHub?")
+  if (PassLength >= 8 && PassLength <= 128){
+    var AlphaLower = confirm("Would you like to include Lowercase letters?");
+    var AlphaUpper = confirm("Would you like to include Uppercase letters?");
+    var Special = confirm("Would you like to include special characters?");
+    var Numerals = confirm("Would you like to include numerals?")
+
+    for (var i = 0; i < PassLength; i++){
+      
+
+    }
+  } else {
+    alert("Invalid Number. \nMust be a number between 8 and 128 \nPlease try again and enter a valid number")
+  }
 }
 
 // Add event listener to generate button
